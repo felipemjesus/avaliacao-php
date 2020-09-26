68b22c8b9d4cf61b94bead08ce395aa30db93185
Felipe Martins <felipemjesus@outlook.com>
2020-09-26 10:48:44 -0300

    Test1 - implementado método que valida o fechamento ou abertura de parêntes, chaves ou cochetes

--- test1/src/BalancedBrackets.php
+++ test1/src/BalancedBrackets.php
@@ -6,12 +6,32 @@ namespace Test1;
 
 class BalancedBrackets
 {
+    private const BRACKETS = ['(' => ')', '{' => '}', '[' => ']'];
+
     /**
      * @param string $text
      * @return bool
      */
     public function validate(string $text): bool
     {
+        $bracketsOpen = array_flip(self::BRACKETS);
+        $bracketsClose = self::BRACKETS;
+
+        $arrText = str_split($text);
+        foreach ($arrText as $bracket) {
+            $isOpen = in_array($bracket, $bracketsOpen);
+
+            // search close bracket
+            if ($isOpen && !in_array($bracketsClose[$bracket], $arrText)) {
+                return false;
+            }
+
+            // search open bracket
+            if (!$isOpen && !in_array($bracketsOpen[$bracket], $arrText)) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
