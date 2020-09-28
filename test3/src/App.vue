9e214f9cffaff46ba3265cd0adb9dd26694e82d1
Felipe Martins <felipemjesus@outlook.com>
2020-09-28 18:03:01 -0300

    Test3 - Instalação do Bootstrap e Bootstrap-Vue

--- test3/src/App.vue
+++ test3/src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
+  <div class="d-flex flex-column h-100" id="app-wrapper">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <main role="main" class="flex-shrink-0 mb-3">
+      <div class="container">
+        <router-view/>
+      </div>
+    </main>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  @import '~bootstrap/scss/bootstrap.scss';
+  @import '~bootstrap-vue/src/index.scss';
 </style>
